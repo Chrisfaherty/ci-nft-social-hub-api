de82917e24c011afb091b103e58191288986ac45
Chrisfaherty <>
2023-04-23 07:22:41 +0000

    Create a PostDetailViewTests and testthat you can retrive a post using a valid id

--- posts/tests.py
+++ posts/tests.py
@@ -26,3 +26,20 @@ class PostListViewTests(APITestCase):
     def test_logged_out_user_can_not_create_post(self):
         response = self.client.post('/posts/', {'title': 'This is a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        sam = User.objects.create_user(username='sam', password='password')
+        mat = User.objects.create_user(username='mat', password='password')
+        Post.objects.create(
+            owner=sam, title='This is a title', content='sams content'
+        )
+        Post.objects.create(
+            owner=mat, title='This is another title', content ='mats content'
+        )
+    
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'],'This is a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
