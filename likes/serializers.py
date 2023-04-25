7704c70f76a42fda1d08e094b3561283efc17c31
Chrisfaherty <>
2023-04-25 07:38:55 +0000

    added create function to LikeSerializer

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,10 +1,10 @@
+from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-
 
     class Meta:
         model = Like
@@ -12,3 +12,10 @@ class LikeSerializer(serializers.ModelSerializer):
             'id', 'owner', 'created_at', 'post',
         ]
     
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+        'detail': 'possible duplicate'
+        })
