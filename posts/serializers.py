fffaf32db3d616b8327604f8d05d6996e48bc505
Chrisfaherty <>
2023-04-25 15:19:06 +0000

    Add the likes_count & comments_count to the posts field

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,6 +10,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -44,5 +46,7 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title', 'content', 'image', 'is_owner', 'image_filter', 'like_id',
+            'id', 'owner', 'profile_id', 'profile_image', 'created_at',
+            'updated_at', 'title', 'content', 'image', 'is_owner',
+            'image_filter', 'like_id', 'likes_count', 'comments_count',
         ]
