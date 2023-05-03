e445908933aa19de53477403bfd57b5336f2ea5f
Chrisfaherty <>
2023-05-03 15:17:34 +0000

    Edit ratings models.py to match likes models

--- ratings/models.py
+++ ratings/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
+from posts.models import Post
 
 
 class Rating(models.Model):
-    image = models.ImageField(upload_to='images/')
-    score = models.IntegerField(default=0,
-        validators=[
-            MaxValueValidator(5),
-            MinValueValidator(0),
-        ]
-    )
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name = 'ratings', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
 
     def __str__(self):
-        return str(self.pk)
+        return f'{self.owner} {self.post}'
