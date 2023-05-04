9543f191ef46ca0140ac5f03dc5ebe4290b5f0df
Chrisfaherty <>
2023-05-04 15:10:08 +0000

    Create the ratings views

--- ratings/views.py
+++ ratings/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import ProductSerializer
-from .models import Post
+from rest_framework import generics, permissions
+from ratings.serializers import RatingSerializer
+from ratings.models import Rating
+from django.http import HttpRequest, HttpResponse
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('?')
-    serializer_class = PostSerializer
+def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.all()
+    for post in posts:
+        rating = Rating.objects.filter(post=post, user=request.user).first()
+        post.user_rating = rating.rating if rating else 0
+    return render(request, "index.html", {"posts": posts})
+
+
+def rate(request: HttpRequest, post_id: int, rating: int) -> HttpResponse:
+    post = Post.objects.get(id=post_id)
+    Rating.objects.filter(post=post, user=request.user).delete()
+    post.rating_set.create(user=request.user, rating=rating)
+    return index(request)
+
+
