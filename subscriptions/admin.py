738e1e7dc6f393327b982ff6e3464fc9f01746d4
Chrisfaherty <>
2023-05-08 13:26:31 +0000

    Register in admin.py and migrate subscribers models.

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Subscribers, SubscribersMessage
 
 # Register your models here.
+
+admin.site.register(Subscribers)
+admin.site.register(SubscribersMessage)
