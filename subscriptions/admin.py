cce50aed29465afcdf1ed394e90cf3aa87bd43c8
Chrisfaherty <>
2023-05-09 19:15:51 +0000

    Remove admin links from subscriptions

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
-from . models import Subscribers, SubscribersMessage
 
-# Register your models here.
-
-admin.site.register(Subscribers)
-admin.site.register(SubscribersMessage)
