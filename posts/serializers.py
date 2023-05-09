a52d6d7603ccd7e6d629b39c1e903acf441a96b5
Chrisfaherty <>
2023-05-09 13:36:30 +0000

    Remove ratings from post fields and add dislike

--- posts/serializers.py
+++ posts/serializers.py
@@ -59,5 +59,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'profile_id', 'profile_image', 'created_at',
             'updated_at', 'title', 'content', 'image', 'is_owner',
-            'category_filter', 'like_id', 'likes_count', 'comments_count', 'rating_id', 'ratings_count',
+            'category_filter', 'like_id', 'likes_count', 'comments_count', 'dislike_id', 'dislikes_count',
         ]
