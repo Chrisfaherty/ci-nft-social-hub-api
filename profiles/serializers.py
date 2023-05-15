03c184014d3781b6bf7078532150217f51c15b2d
Chrisfaherty <chrismactom@gmail.com>
2023-05-15 18:23:52 +0000

    Annotate the profiledetail view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Profile
 from followers.models import Follower
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
@@ -14,7 +15,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
     
-    def get_following_id(self,obj):
+    def get_following_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
             following = Follower.objects.filter(
@@ -22,7 +23,6 @@ class ProfileSerializer(serializers.ModelSerializer):
             ).first()
             return following.id if following else None
         return None
-
 
     class Meta:
         model = Profile
