8f5d464ea06998ec303549230e957aa48366791d
Chrisfaherty <chrismactom@gmail.com>
2023-05-22 07:36:43 +0000

    Edit the subscriptions models and serializers

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,27 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Subscribers(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=55)
     email = models.EmailField(null=True)
     date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ['owner', 'email']
+        unique_together = ['fullname', 'email']
 
     def __str__(self):
-        return f'{self.owner} {self.email}'
+        return f'{self.fullname} {self.email}'
 
 
 class SubscribersMessage(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    fullname = models.models.CharField(max_length=55)
+    email = models.EmailField(null=True)
     title = models.CharField(max_length=100, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    message = models.TextField(null=True)
+    message = models.TextField(blank=True)
 
     class Meta:
-        unique_together = ['owner', 'title']
+        unique_together = ['fullname', 'email']
 
     def __str__(self):
-        return f'{self.owner} {self.title}'
+        return f'{self.fullname} {self.email}'
--- subscriptions/serializers.py
+++ subscriptions/serializers.py
@@ -3,23 +3,18 @@ from subscriptions.models import Subscribers, SubscribersMessage
 
 
 class SubscribersSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.name')
-    email = serializers.EmailField()
 
     class Meta:
         model = Subscribers
         fields = [
-            'id', 'owner', 'created_at', 'email', 'date'
+            'id', 'fullname', 'created_at', 'email', 'date'
         ]
 
 
 class SubscribersMessageSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.name')
-    title = serializers.CharField()
-    message = serializers.CharField()
 
     class Meta:
-        model = Subscribers
+        model = SubscribersMessage
         fields = [
-            'id', 'owner', 'created_at', 'title', 'message'
+            'id', 'fullname', 'created_at', 'title', 'message', 'email',
         ]
